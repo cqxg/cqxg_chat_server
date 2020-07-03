a1dc8fc374717c78f7644cb84faae4af0dd440ad
unknown <cqxg@yandex.ru>
2020-07-03 19:28:14 +0300

    fix: simple fixes

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ io.on('connect', (socket) => {
     const user = removeUser(socket.id);
 
     if(user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` });
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     }
   })
