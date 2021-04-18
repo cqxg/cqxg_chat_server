40db46e71589ed19444b8a8926f5d8dd5a8b68b5
cqxg <cqxg@yandex.ru>
2021-04-18 12:07:32 +0300

    fix: CORS policy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const cors = require('cors');
+const cors = require('no-cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
