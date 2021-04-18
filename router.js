5c53773806221e9cb9e7def9b4269d367dea5303
cqxg <cqxg@yandex.ru>
2021-04-18 13:57:22 +0300

    fix: cors

--- router.js
+++ router.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require('cors');
+
 const router = express.Router();
+
+router.use(cors());
 
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
